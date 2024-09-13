48bc5379468017f6251a77fdb14c515da754bc52
İsmail Özer <ismailoze@vestel.com.tr>
2024-09-13 10:47:33 +0300

    Grpc phase 1

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,14 @@
+﻿global using BuildingBlocks.CQRS;
+global using Carter;
+global using Catalog.API.Models;
+global using Mapster;
+global using Marten;
+global using MediatR;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Data;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler
+        (IDocumentSession session)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            var products = await session.Query<Product>()
+                .Where(w => w.Category.Contains(query.Category))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
